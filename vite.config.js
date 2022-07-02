6f36c2a2d546e1f5077a7019f7d6d79f5eb5a457
Daniel RB <daniel@danielrb.dev>
2022-07-02 18:58:10 -0400

    feat: install vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    compilerOptions: {
-      dev: true
-    }
-  })],
-  mode: 'development'
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    }),
+  ],
 })
