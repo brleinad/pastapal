03d7dad8bba4ad78df267125466c9f787cdbec17
Daniel RB <daniel@danielrb.dev>
2022-07-02 19:05:37 -0400

    feat: add icon

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,20 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      includeAssets: ['public/favicon.ico', 'public/pastapal.icon-512.png'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pastapa-icon--512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     }),
   ],
